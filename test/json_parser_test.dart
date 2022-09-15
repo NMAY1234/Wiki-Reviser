a194ed4fe086c78c851293d056e0e1e45a1a90af
NMMAY1234 <nmattmay@gmail.com>
2022-09-15 18:54:27 -0400

    Tested parsing of sample json text file to pull out revision user name and time stamp in UTC.

--- test/json_parser_test.dart
+++ test/json_parser_test.dart
@@ -1,6 +1,7 @@
 import 'package:test/test.dart';
 import 'dart:convert';
 import 'dart:io';
+import 'package:http/http.dart';
 
 void main() {
   test('test data opened', () async {
@@ -16,7 +17,12 @@ void main() {
     final mapData = jsonDecode(jsonFile);
 
     final page1 = mapData["query"]["pages"].entries.first.value;
-    final answer = page1["revisions"][0]["user"];
-    expect(answer, 'Tom.Reding');
+    final revisionUserName = page1["revisions"][0]["user"];
+    final revisionTimeStamp = page1["revisions"][0]["timestamp"];
+
+    expect(revisionUserName, 'Tom.Reding');
+    expect(revisionTimeStamp, '2017-06-06T02:24:07Z');
   });
+
+  test('this is a json file', () async {});
 }
